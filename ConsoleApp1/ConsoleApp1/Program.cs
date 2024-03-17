7d0a459c3e5b2b29714206bd234d5bc6b1a237a0
s26874 <s26874@pjwstk.edu.pl>
2024-03-18 00:26:02 +0100

    New function feature-average created

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Modification 1");
-Console.WriteLine("Modification 3");git
+Console.WriteLine("Modification 3");
+
+static int ArrAverage(int[] arr)
+{
+    int res = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        res += arr[i];
+    }
+    return res;
+}
